817e9c2683ebe0c63db70108ccb1a7f25c587b7a
Hafeed10 <hafeex@gamil.com>
2025-10-01 22:09:25 +0530

    project item removes and new add

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Title from '../layouts/Title';
 import ProjectsCard from '../project/ProjectCard';
 import './Project.css';
-import { Link } from 'react-router-dom';
+
 import localProjects from './ProductData'; 
 const Projects = () => {
   // Local project data
@@ -17,7 +17,7 @@ const Projects = () => {
         />
       </div>
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-10">
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-10">
         {localProjects.length > 0 ? (
           localProjects.map((project, index) => (
             <ProjectsCard
@@ -34,11 +34,6 @@ const Projects = () => {
             No projects available at the moment.
           </p>
         )}
-      </div>
-      <div className="flex justify-end items-center mt-10 cursor-pointer">
-       <Link to="/viewprojects" className='bg-designColor p-2 px-6 rounded-lg text-white font-semibold hover:bg-opacity-80 duration-300' smooth={true} duration={500}>
-         View All Projects
-       </Link>
       </div>
     </section>
   );
--- src/components/project/ViewProjects.jsx
+++ src/components/project/ViewProjects.jsx
@@ -1,36 +1,9 @@
-import React from "react";
-import Title from "../layouts/Title";
-import ProjectsCard from "./ProjectCard";
-import local from "./ProData"; 
+import React from 'react'
 
-const ViewProjects = () => {
-
+function ViewProjects() {
   return (
-    <section className="w-full py-20">
-      <div className="flex justify-center items-center text-center mb-10">
-        <Title title="ALL PROJECTS" des="Explore My Full Portfolio" />
-      </div>
+    <div>ViewProjects</div>
+  )
+}
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-10">
-        {local.length > 0 ? (
-          local.map((project, index) => (
-            <ProjectsCard
-              key={index}
-              title={project.title}
-              src={project.src}
-              link={project.link}
-              github={project.github}
-              des={project.des}
-            />
-          ))
-        ) : (
-          <p className="text-center text-gray-500 mt-6">
-            No projects available at the moment.
-          </p>
-        )}
-      </div>
-    </section>
-  );
-};
-
-export default ViewProjects;
+export default ViewProjects
